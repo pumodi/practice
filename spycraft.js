1a9b653d00c7d6fb98672589338eaaa05d54d1ca
pumodi <jeff@jeffbricemusic.com>
2019-12-19 12:25:02 -0800

    Added spycraft character rolling javascript.

---
+++ spycraft.js
@@ -0,0 +1,15 @@
+// Spycraft 2.0 - Charlotte Beuregard
+
+var int str;
+var int con;
+var int dex;
+var int int;
+var int wis;
+var int cha;
+
+function rollDice() {
+	
+}
+
+function computerSkill(int mod) {
+}
